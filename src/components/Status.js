169520da76933dbc13f24389946a3742868e4e2f
Jethro Duque <144524582+eldusastig@users.noreply.github.com>
2025-08-28 03:39:53 +0800

    Update Status.js

--- src/components/Status.js
+++ src/components/Status.js
@@ -171,7 +171,7 @@ export default function Status() {
     const tsStr = log.ts ? formatLogTimestamp(log, device) : '—';
     const classes = log.classes &&(
       (Array.isArray(log.classes) && log.classes.length > 0 ) ||
-      (typeof log.classes== 'string' && log.classes.trim() !== '')
+      (typeof log.classes== 'string' && log.classes.trim().toLowerCase() !== 'none' && log.classes.trim()!== '')
     )
      ?'Rubbish Detected'
      : 'None' ;
