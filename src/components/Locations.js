eaaffafd47854953d089cd4eabcc4fa9ffd041d2
Dhafny Buenafe <157882179+dsbuenafe@users.noreply.github.com>
2026-03-21 14:41:31 +0800

    Update Locations.js

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -289,6 +289,7 @@ const s = StyleSheet.create({
     marginBottom: '20px',
     border: '1px solid rgba(255,255,255,0.06)',
     boxShadow: '0 4px 16px rgba(0,0,0,0.3)',
+    marginLeft: '5px',
   },
   listCard: {
     backgroundColor: '#1E293B',
